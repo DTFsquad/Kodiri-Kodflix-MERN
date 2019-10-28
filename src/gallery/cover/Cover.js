f2de247a86259b150b55e52f6ed5bbd4a1252efc
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-10-28 21:43:27 +0000

    Keep a unique details URL for each TV show

--- src/gallery/cover/Cover.js
+++ src/gallery/cover/Cover.js
@@ -7,7 +7,7 @@ import './Cover.css';
 export default class Cover extends React.Component {
     render() {
         return (
-            <Link to='/details' className='cover'>
+            <Link to={`/${this.props.id}`} className='cover'>
                 <img src={this.props.image} alt='' />
                 <div className='cover-overlay'>
                     <h1>{this.props.title}</h1>
