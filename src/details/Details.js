150aec92594b37e643ad33e8573783cabfbde8e3
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-10-28 22:13:46 +0000

    Introduce component state

--- src/details/Details.js
+++ src/details/Details.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './Details.css';
 
 export default class Details extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            message: 'Hello, this will be the details page for each Movie & TV show'
+        };
+    }
+
+        componentDidMount() {
+            setTimeout(() => {
+                this.setState({message: 'Coming soon!'});
+            }, 3000);
+        }
     render() {
         return (
-            <h1>
-                Hello, this will be the details page for each movie & TV show
-            </h1>
+            <h1>{this.state.message}</h1>
         )
     }
 }
