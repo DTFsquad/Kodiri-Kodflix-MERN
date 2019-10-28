02285049e72f319a4a0c45614ac3e0a7222a2b2a
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-10-28 21:36:07 +0000

    Introduce routing / navigation

---
+++ src/details/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Details.css';
+
+export default class Details extends React.Component {
+    render() {
+        return (
+            <h1>
+                Hello, this will be the details page for each movie & TV show
+            </h1>
+        )
+    }
+}
--- src/gallery/cover/Cover.js
+++ src/gallery/cover/Cover.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Cover.css';
 
 
@@ -6,12 +7,12 @@ import './Cover.css';
 export default class Cover extends React.Component {
     render() {
         return (
-            <div className='cover'>
+            <Link to='/details' className='cover'>
                 <img src={this.props.image} alt='' />
                 <div className='cover-overlay'>
                     <h1>{this.props.title}</h1>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
